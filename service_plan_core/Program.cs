82dd927721fd486e136bc602a1d32816debd18ef
atchapcyp <atcha.crypto@gmail.com>
2018-09-12 14:28:45 +0700

    Try to run after import class SUCCESS

--- service_plan_core/Program.cs
+++ service_plan_core/Program.cs
@@ -9,6 +9,23 @@ namespace service_plan_core
             Console.WriteLine("Initiate Service Planning .net core");
             Console.WriteLine("Test 1st editing and commit");
             Console.WriteLine("Test 2nd editting and commit on macbook ");
+
+            int[,] passeng = new int[5, 5];
+            Train_obj train = new Train_obj(300);        
+            Service_algo.make5x5(passeng);
+            Service_algo.showarray(passeng);
+            Console.WriteLine("------------------ ");
+            Service_algo.train_a_b_c_d_e(passeng, train);
+            Service_algo.showarray(passeng);
+            Console.WriteLine("------------------ ");
+            Service_algo.train_a_b_c_d_e(passeng, train);
+            Service_algo.showarray(passeng);
+            Console.WriteLine("------------------ ");
+            Service_algo.train_a_b_c_d_e(passeng, train);
+            Service_algo.showarray(passeng);
+            Console.WriteLine("------------------ ");
+            Service_algo.train_a_b_c_d_e(passeng, train);
+            Service_algo.showarray(passeng);
         }
     }
 }
