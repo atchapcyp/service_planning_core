c3f8618b6e6ab0d9355d252742e8786e3bcec62f
atchapcyp <atcha.crypto@gmail.com>
2018-09-12 14:23:51 +0700

    add Train_obj class

---
+++ service_plan_core/Train_obj.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace service_plan_core
+{
+    class Train_obj
+    {
+        public int cap = 150;
+        public int remain_cap = 150;
+        public Train_obj()
+        { }
+        public Train_obj(int c)
+        {
+            cap = c;
+            remain_cap = cap;
+        }
+    }
+}
