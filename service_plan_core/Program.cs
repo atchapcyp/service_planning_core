51f577cbaf5178ff4f3dec97aee14d1cb40826c8
atchapcyp <atcha.crypto@gmail.com>
2018-09-14 16:04:45 +0700

    unfinish split5x5 funcion

--- service_plan_core/Program.cs
+++ service_plan_core/Program.cs
@@ -8,12 +8,14 @@ namespace service_plan_core
     {
         static void Main(string[] args)
         {
+            //enum direction {a,b};
             Console.WriteLine("Initiate Service Planning .net core");
             Console.WriteLine("Test 1st editing and commit");
             Console.WriteLine("Test 2nd editting and commit on macbook ");
 
             int[,] passeng = new int[5, 5];
-            List<int[]> myList = new List<int[]>();
+            List<int[]> forward = new List<int[]>();
+            List<int[]> backward = new List<int[]>();
             Train_obj train = new Train_obj(7);
             int[] service = { 1, 1, 1, 1, 1 };
             int[] service2 = { 1, 1, 1, 0, 1 };
@@ -23,21 +25,32 @@ namespace service_plan_core
             int[] opservice2 = { 1, 1, 1, 0, 1 };
             int[] opservice3 = { 1, 0, 0, 0, 1 };
  
-            myList.Add(service);
-            myList.Add(service2);
-            myList.Add(service3);
+            //add service to list
+            forward.Add(service);
+            forward.Add(service2);
+            forward.Add(service3);
+            backward.Add(opservice);
+            backward.Add(opservice2);
+            backward.Add(opservice3);
 
 
             Service_algo.Make5x5(passeng);
+            Console.WriteLine("This is all station demand . ");
+            Service_algo.showarray(passeng);
+
+            Console.WriteLine("------------------ ");
+            Service_algo.Train_a_b_c_d_e(passeng, train,forward[1]); 
             Service_algo.showarray(passeng);
             Console.WriteLine("------------------ ");
-            Service_algo.Train_a_b_c_d_e(passeng, train,myList[1]); 
-            Service_algo.showarray(passeng);
-           
-            Console.WriteLine("------------------ ");
-            Service_algo.Train_a_b_c_d_e(passeng, train, myList[2]);
+            Service_algo.Train_a_b_c_d_e(passeng, train, forward[2]);
             //Service_algo.train_x_y(passeng, train, 0, 3);
             Service_algo.showarray(passeng);
+
+
+
+
+
+            
 
 
             //unused
@@ -46,5 +59,20 @@ namespace service_plan_core
             //int[,] service1 =new int[2,5] { { 1, 1, 1, 1, 1 }, { 1, 0, 1, 0, 1 } };
 
         }
+
+
+        public static int[,] split5x5(int[,] fullmatrix, char direction)
+        { int[,] a= {{ 0, 10, 10, 10, 10 },
+            { 10, 0, 10, 10, 10 },
+            { 10, 10, 0, 10, 10 },
+            { 10, 10, 10, 0, 10 },
+            { 10, 10, 10, 10, 0 }};
+
+           // if direction
+
+
+            return a;
+        }
     }
+
 }
